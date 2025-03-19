53dd7098b1c68ee0232406a7b1d00917de728642
ja9ongz <ja9ongz@gmail.com>
2025-03-20 04:03:37 +0800

    removed cached files

--- backend/api/utils/tokens.py
+++
@@ -1,7 +0,0 @@
-from rest_framework_simplejwt.tokens import RefreshToken
-
-def create_refresh_token(user):
-    refresh = RefreshToken()
-    refresh['user_id'] = user.uid  
-    print(f"refresh {refresh['user_id']}")
-    return refresh
